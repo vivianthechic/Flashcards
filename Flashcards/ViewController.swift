0ba715f55d4f4bbf4e50e48db508ede667ea0145
Vivian Zhang <vszhang@uci.edu>
2018-10-14 16:41:00 -0700

    Optional features added

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -12,9 +12,26 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var card: UIView!
+    @IBOutlet weak var btnOptionOne: UIButton!
+    @IBOutlet weak var btnOptionTwo: UIButton!
+    @IBOutlet weak var btnOptionThree: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        card.layer.cornerRadius = 20.0
+        frontLabel.layer.cornerRadius = 20.0
+        backLabel.layer.cornerRadius = 20.0
+        card.layer.shadowRadius = 15.0
+        card.layer.shadowOpacity = 0.2
+        frontLabel.clipsToBounds = true
+        backLabel.clipsToBounds = true
+        btnOptionOne.layer.borderWidth = 3.0
+        btnOptionOne.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        btnOptionTwo.layer.borderWidth = 3.0
+        btnOptionTwo.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        btnOptionThree.layer.borderWidth = 3.0
+        btnOptionThree.layer.borderColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -24,8 +41,24 @@ class ViewController: UIViewController {
     }
     
     @IBAction func didTapOnFlashcard(_ sender: Any) {
-        frontLabel.isHidden = true
+        if frontLabel.isHidden {
+            frontLabel.isHidden = false
+        }
+        else{
+            frontLabel.isHidden = true
+        }
     }
     
+    @IBAction func didTapOptionOne(_ sender: Any) {
+        btnOptionOne.isHidden = true
+    }
+    
+    @IBAction func didTapOptionTwo(_ sender: Any) {
+        btnOptionTwo.isHidden = true
+    }
+    
+    @IBAction func didTapOptionThree(_ sender: Any) {
+        frontLabel.isHidden = true
+    }
 }
 
--- readme.md
+++ readme.md
@@ -4,9 +4,11 @@
 Simple application that shows a question and presents an answer when tapped.
 
 ### App Walk-though
-
+Required:
 <img src="https://media.giphy.com/media/MX4xODo2aMTw1xeoAq/giphy.gif" width=200><br>
-
+Optional:
+<img src="https://media.giphy.com/media/7zy0cLhMkXJfuKxdIe/giphy.gif" width=200><br>
+<img src="https://media.giphy.com/media/3HBR7AfIZoqAl2e10Y/giphy.gif" width=200><br>
 
 ## Required
 - [x] Create New Project in Xcode
@@ -15,6 +17,6 @@ Simple application that shows a question and presents an answer when tapped.
 - [x] Build in logic to show the answer side when the card is tapped
 - [x] Push code to GitHub
 ## Optional
-- [ ] Toggle the flashcard between the question side and the answer side
-- [ ] Style the question and answer side of the card to better distinguish between the two sides
-- [ ] Add selectable multiple choice answers beneath the card
+- [x] Toggle the flashcard between the question side and the answer side
+- [x] Style the question and answer side of the card to better distinguish between the two sides
+- [x] Add selectable multiple choice answers beneath the card
